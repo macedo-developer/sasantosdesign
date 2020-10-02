b00b46051de64e4cd4314a4245489c1278f93660
macedo-developer <macedorenata.developer@gmail.com>
2020-10-02 16:59:07 -0300

    Script the page home

--- js/home.js
+++ js/home.js
@@ -1,7 +1,67 @@
-const projects = document.querySelectorAll(".projects li");
+const projectBrendaDiv = document.querySelector(".brenda");
+const pBrenda = document.querySelector(".brenda .detail-project p");
 
-projects.forEach((project) => {
-  project.addEventListener("onmouseover", () => {
-    console.log("oi");
-  });
-});
+projectBrendaDiv.onmouseover = () => {
+  pBrenda.style.display = "block";
+};
+
+projectBrendaDiv.onmouseleave = () => {
+  pBrenda.style.display = "none";
+};
+
+const projectAdrianoDiv = document.querySelector(".adriano");
+const pAdriano = document.querySelector(".adriano .detail-project p");
+
+projectAdrianoDiv.onmouseover = () => {
+  pAdriano.style.display = "block";
+};
+
+projectAdrianoDiv.onmouseleave = () => {
+  pAdriano.style.display = "none";
+};
+
+const projectMesomorfoDiv = document.querySelector(".mesomorfo");
+const pMesomorfo = document.querySelector(".mesomorfo .detail-project p");
+
+projectMesomorfoDiv.onmouseover = () => {
+  pMesomorfo.style.display = "block";
+};
+
+projectMesomorfoDiv.onmouseleave = () => {
+  pMesomorfo.style.display = "none";
+};
+
+const projectMaria_clemuraDiv = document.querySelector(".maria_clemura");
+const pMaria_clemura = document.querySelector(
+  ".maria_clemura .detail-project p"
+);
+
+projectMaria_clemuraDiv.onmouseover = () => {
+  pMaria_clemura.style.display = "block";
+};
+
+projectMaria_clemuraDiv.onmouseleave = () => {
+  pMaria_clemura.style.display = "none";
+};
+
+const projectBigoloDiv = document.querySelector(".bigolo");
+const pBigolo = document.querySelector(".bigolo .detail-project p");
+
+projectBigoloDiv.onmouseover = () => {
+  pBigolo.style.display = "block";
+};
+
+projectBigoloDiv.onmouseleave = () => {
+  pBigolo.style.display = "none";
+};
+
+const projectGepossDiv = document.querySelector(".geposs");
+const pGeposs = document.querySelector(".geposs .detail-project p");
+
+projectGepossDiv.onmouseover = () => {
+  pGeposs.style.display = "block";
+};
+
+projectGepossDiv.onmouseleave = () => {
+  pGeposs.style.display = "none";
+};
